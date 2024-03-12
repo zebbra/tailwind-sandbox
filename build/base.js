94c61d2802a03d7227c8d4dcc8844b7fc5cf1abc
zebbraluchs <lukas.vanazzi@zebbra.ch>
2024-03-12 15:54:58 +0100

    Div. modifications

---
+++ build/base.js
@@ -0,0 +1,11 @@
+function showHideDiv(d) {
+    d.classList.toggle("hidden");
+}
+
+function showDiv(d) {
+    d.classList.remove("hidden");
+}
+
+function hideDiv(d) {
+    d.classList.add("hidden");
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
-  content: ["./**/*.{html,js}"],
-  content: ["./components/**/*.{html,js}"],
+  content: ["./*.{html,js}",
+  "./**/*.{html,js}",
+  "./components/**/*.{html,js}"],
   plugins: [require("daisyui")],
   // config docs: https://daisyui.com/docs/config/
   theme: {
@@ -31,20 +31,14 @@ module.exports = {
           'base-100': '#ffffff',
           'base-200': '#F9FAFA',
           'base-300': '#EEEFEF',
-          'base-400': '#E3E4E4',
-          'base-500': '#D8D9D9',
-          'base-600': '#F4F4F4',
-          'base-700': '#C7C8C9',
-          'base-800': '#8B8C8D',
-          'base-900': '#505050',
           'base-content': '#1f2329',
           'info': '#8BAED9',
           'success': '#95d98a',
           'warning': '#D9BD8B',
           'error': '#e11d48',
         },
-
       },
+      "dark",
     ],
   },
 };
